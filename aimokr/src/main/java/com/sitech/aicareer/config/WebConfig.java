62968fd1b900c9873ee584cb19e09e15470e5c17
wangjn_bj <wangjn_bj@si-tech.com.cn>
2022-01-19 10:57:00 +0800

    解决跨域问题

--- aimokr/src/main/java/com/sitech/aicareer/config/WebConfig.java
+++ aimokr/src/main/java/com/sitech/aicareer/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 
 @Configuration
 public class WebConfig {
@@ -24,7 +25,7 @@ public class WebConfig {
         FilterRegistrationBean<CommonFilter> bean = new FilterRegistrationBean<CommonFilter>();
         bean.setFilter(new CommonFilter());
         bean.addUrlPatterns("/*");
-        bean.setOrder(1);
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
         return bean;
     }
 }
